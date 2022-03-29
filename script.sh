d0d1074dfff3fcc6bc1fdc34b120459e624690ce
Irungaray <lionelirungaray@gmail.com>
2022-03-29 20:16:03 -0300

    A demonstration of my (not-so) holistic skills on bash scripting

---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+files=(
+	~/.zshrc
+	
+	# Regolith config
+	/etc/regolith/i3/config
+	~/.config/regolith/Xresources 
+
+	# Wallpaper
+	/usr/share/backgrounds/irungapaper.jpg
+
+	# Ranger
+	~/.config/ranger/rc.conf
+
+	# Vim
+	~/.vimrc
+
+	# VSCodium
+	/home/irunga/.config/VSCodium/User/keybindings.json
+	/home/irunga/.config/VSCodium/User/settings.json
+	/home/irunga/.config/VSCodium/User/snippets/react-snippets.code-snippets
+
+	# Left: keybindings
+)
+
+for n in ${files[@]}
+do
+	echo "Copying $n..."
+	cp $n ./dotfiles
+done
+
+echo "Succesfully copied all your dotfiles, capo! salu3"
+
