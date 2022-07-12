a322f72c82ee7d3cdb7b6dc566eceddf74d33274
Irungaray <62315823+Irungaray@users.noreply.github.com>
2022-07-12 18:29:28 -0300

    Richard Stallman would be proud

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 files=(
+
 	~/.zshrc
-	
+
 	# Regolith config
 	/etc/regolith/i3/config
-	~/.config/regolith/Xresources 
+	~/.config/regolith/Xresources
 
 	# Wallpaper
 	/usr/share/backgrounds/irungapaper.jpg
@@ -21,7 +22,13 @@ files=(
 	/home/irunga/.config/VSCodium/User/settings.json
 	/home/irunga/.config/VSCodium/User/snippets/react-snippets.code-snippets
 
+	# VSCode (like a normie)
+	/home/irunga/.config/Code/User/keybindings.json
+	/home/irunga/.config/Code/User/settings.json
+	/home/irunga/.config/Code/User/snippets/react-snippets.code-snippets
+
 	# Left: keybindings
+
 )
 
 for n in ${files[@]}
@@ -31,4 +38,3 @@ do
 done
 
 echo "Succesfully copied all your dotfiles, capo! salu3"
-
