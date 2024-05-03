c70e872235d087613a470129cf42e2e7d2951aaa
Lionel Irungaray <104365127+Irungaray-AiC@users.noreply.github.com>
2024-05-03 17:30:40 -0300

    Fix on symlink

--- sym_link.sh
+++ sym_link.sh
@@ -3,34 +3,31 @@
 echo "Starting to symlink all dotfiles..."
 
 # ZSH
-ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/dotfiles/.zshrc ~/.zshrc
 
 # Regolith config
-ln -s -f ~/dotfiles/config /etc/regolith/i3/config
-ln -s -f ~/dotfiles/Xresources ~/.config/regolith/Xresources
+ln -s -f ~/dotfiles/dotfiles/config /etc/regolith/i3/config
+ln -s -f ~/dotfiles/dotfiles/Xresources ~/.config/regolith/Xresources
 
 # Ranger
-ln -s -f ~/dotfiles/rc.conf ~/.config/ranger/rc.conf
-ln -s -f ~/dotfiles/scope.sh ~/.config/ranger/scope.sh
+ln -s -f ~/dotfiles/dotfiles/rc.conf ~/.config/ranger/rc.conf
+ln -s -f ~/dotfiles/dotfiles/scope.sh ~/.config/ranger/scope.sh
 
 # Vim
-ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/dotfiles/.vimrc ~/.vimrc
 
 # Compton
-ln -s -f ~/dotfiles/compton.conf ~/.config/compton.conf
-
-# Ranger
-ln -s -f ~/dotfiles/scope.sh ~/.config/ranger/scope.sh
+ln -s -f ~/dotfiles/dotfiles/compton.conf ~/.config/compton.conf
 
 # VSCodium
-ln -s -f ~/dotfiles/keybindings.json ~/.config/VSCodium/User/keybindings.json
-ln -s -f ~/dotfiles/settings.json ~/.config/VSCodium/User/settings.json
-ln -s -f ~/dotfiles/react-snippets.code-snippets ~/.config/VSCodium/User/snippets/react-snippets.code-snippets
+ln -s -f ~/dotfiles/dotfiles/keybindings.json ~/.config/VSCodium/User/keybindings.json
+ln -s -f ~/dotfiles/dotfiles/settings.json ~/.config/VSCodium/User/settings.json
+ln -s -f ~/dotfiles/dotfiles/react-snippets.code-snippets ~/.config/VSCodium/User/snippets/react-snippets.code-snippets
 
 # VSCode (like a normie)
-ln -s -f ~/dotfiles/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s -f ~/dotfiles/settings.json ~/.config/Code/User/settings.json
-ln -s -f ~/dotfiles/react-snippets.code-snippets ~/.config/Code/User/snippets/react-snippets.code-snippets
+ln -s -f ~/dotfiles/dotfiles/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s -f ~/dotfiles/dotfiles/settings.json ~/.config/Code/User/settings.json
+ln -s -f ~/dotfiles/dotfiles/react-snippets.code-snippets ~/.config/Code/User/snippets/react-snippets.code-snippets
 
 # Autokey Keybindings
 ln -s -f ~/dotfiles/Tlgrm.py ~/hotkeys/Tlgrm.py
@@ -39,6 +36,6 @@ ln -s -f ~/dotfiles/Tlgrm.py ~/hotkeys/Tlgrm.py
 echo "Copying wallpapers..."
 
 mkdir ~/Pictures/wallpapers
-cp -R ./dotfiles/wallpapers ~/Pictures/wallpapers
+cp -R ~/dotfiles/dotfiles/wallpapers ~/Pictures/wallpapers
 
 echo "Succesfully symlinked all dotfiles and copied wallpapers. Salu3, capo!"
